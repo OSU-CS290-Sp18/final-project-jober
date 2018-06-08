cee46b927b4ae75db3f83851582a29435582462c
Steven Wells <stevewells20@gmail.com>
2018-06-07 20:08:51 -0700

    Working database insert from client side

--- Server/server.js
+++ Server/server.js
@@ -2,17 +2,22 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var DB = require('./../DB/DB-interface');
 
 var contractData = require('./contractData');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.get('/style.css', function (req, res, next) {
     res.status(200).sendFile(__dirname + '/Styles/style.css');
@@ -26,6 +31,17 @@ app.get('/main', function (req, res, next) {
     res.status(200).render('homePage', {
         contracts: contractData
     });
+});
+
+app.post('/submitJob', function(req, res, next) {
+  console.log(req.body);
+  DB.insertNew('jobs', req.body).then((result) => {
+      console.log(result);
+      console.log('Record created!');
+    }).catch((err) => {
+      console.log(err);
+  });
+
 });
 
 app.listen(port, function () {
